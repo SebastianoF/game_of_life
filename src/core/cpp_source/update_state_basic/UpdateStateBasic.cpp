15699b3ac1593c0cc3388c1f9772fa408aebbbef
SebastianoF <sebastiano.ferraris@gmail.com>
2016-08-13 20:55:52 +0100

    small modifications

--- src/core/cpp_source/update_state_basic/UpdateStateBasic.cpp
+++ src/core/cpp_source/update_state_basic/UpdateStateBasic.cpp
@@ -11,57 +11,13 @@ Tests are provided in the Python embedding, and performed with nosetests.
 #include <string>
 #include <sstream>
 
-class GameManager
+class StateManager
 {
-	private:
-		std::string path_to_seed;
-		int max_num_iteration;
-				
-    public:
-        SomeClass(std::string n) : name(n), mNumber(0.0) {}
-
-        std::string name;
-
-        double getNumber() const { return mNumber; }
-
-        void setNumber(double n)
-        {
-            if (n>3.141592654)
-                n = -1;
-            mNumber = n;
-        }
-
-    private:
-        double mNumber;
+    // TODO here
 };
 
 
-#include <boost/python.hpp>
-using namespace boost::python;
 
-BOOST_PYTHON_MODULE(member)
-{
-    class_<GameManager>("SomeClass", init<std::string>())
-        .def_readwrite("name", & SomeClass::name)
-        .add_property("number", &SomeClass::getNumber, &SomeClass::setNumber)
-    ;
-
-}
-
-
-
-
-class Rectangle {
-    int width, height;
-  public:
-    void set_values (int,int);
-    int area() {return width*height;}
-};
-
-void Rectangle::set_values (int x, int y) {
-  width = x;
-  height = y;
-}
 
 
 
